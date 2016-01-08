0353b4d9c0615d606680e3014d8745f6c4c20079
idanbe <idan_beck@walla.com>
2016-01-08 12:34:02 +0200

    update

--- Home_Ex3/app/src/main/java/com/example/administrator/home_ex3/MainActivity.java
+++ Home_Ex3/app/src/main/java/com/example/administrator/home_ex3/MainActivity.java
@@ -2,15 +2,51 @@ package com.example.administrator.home_ex3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private String loction_selcted ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View selectedItemView, int pos, long id) {
+
+                loction_selcted = parent.getItemAtPosition(pos).toString();
+
+                Log.d("@@@",loction_selcted);
+
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
+
+
+
+
     }
 
     @Override
