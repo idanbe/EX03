f5cdb01c0667cd7b34b919944b279042cacbda19
idanbe <idan_beck@walla.com>
2016-01-10 10:57:41 +0200

    update

--- Home_Ex3/app/src/main/java/com/example/administrator/home_ex3/MainActivity.java
+++ Home_Ex3/app/src/main/java/com/example/administrator/home_ex3/MainActivity.java
@@ -22,6 +22,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements LocationListener
@@ -36,8 +37,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener
     private final long MIN_DISTANCE = 5;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +44,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener
 
         context = this;
         spinner = (Spinner) findViewById(R.id.spinner);
-
 
         //init location
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
@@ -64,13 +62,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener
                     //// TODO: get location
 
 
-                    boolean isGPSAvailable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-                    if (isGPSAvailable) {
 
-                        //get GPS updates
-                      //  locationManager.requestLocationUpdates(locationManager.getProvider());
-
-                    }
 
                 }
                 get_forecast(loction_selcted);
@@ -88,23 +80,61 @@ public class MainActivity extends AppCompatActivity implements LocationListener
 
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 
     private void get_forecast(String location)
     {
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://api.openweathermap.org/data/2.5/weather?q="+location+"&appid=2de143494c0b295cca9337e1e96b00e0";
+        String url = "http://api.openweathermap.org/data/2.5/forecast?q="+location+"&units=metric"+"&appid=2de143494c0b295cca9337e1e96b00e0";
 
 
         Log.d("@@@222",location);
 
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+        final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try
                 {
-                    Log.d("@@@222",response.toString()); //all data
+                   // Log.d("@@@222",response.toString()); //all data
 
                   //todo parse on data respnonse
+
+                    //Log.d("@@@222","before joke");
+                    JSONArray jokes = response.getJSONArray("list");
+
+                   // Log.d("@@@222","afther joke:"+jokes.toString());
+                    String s1="d";
+                    if (jokes.length() > 0)
+                    {
+                        JSONObject json =jokes.getJSONObject(1);
+                        Log.d("@@@222","json"+json.toString());
+                        try {
+                             s1 = json.getString("temp");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                     //   Log.d("@@@222",s1); //all data
+                     //   Log.d("@@@222","in if"); //all data
+                    }
+                     //   Log.d("@@@222","out if"); //all data
+
+
+
+
+
 
                 } catch (Exception e) {
                     e.printStackTrace();
